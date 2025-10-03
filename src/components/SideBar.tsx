7dc7b193c0401d568dfefae19dbe5c8f94f13874
Kanta Nagai <93016258+kankan70717@users.noreply.github.com>
2025-10-03 11:09:46 -0700

    header-sideBar-component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,28 +1,56 @@
 import { faGear, faHouse, faMagnifyingGlass, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "@tanstack/react-router";
+import type { Dispatch, SetStateAction } from "react";
+import siteLogoWithText from "../assets/siteLogo_text.png";
+import siteLogo from "../assets/siteLogo.png";
 
-export function SideBar({ isSidebarOpen }: { isSidebarOpen: boolean }) {
+export function SideBar({
+	isSidebarOpen,
+	setSidebarOpen
+}: {
+	isSidebarOpen: boolean,
+	setSidebarOpen: Dispatch<SetStateAction<boolean>>
+}) {
 
 	return (
-		<div id="sideBar" className={`relative flex flex-col items-center gap-3 px-5 border-r-1 border-[var(--color-border-lightGray)] ease-in-out duration-500 ${isSidebarOpen ? "w-50 items-start" : "w-20"}`}>
-			<div className="flex flex-col items-center gap-3 py-20 w-full">
-				<div className={`capitalize w-10 h-10 rounded-full flex justify-center items-center gap-2 hover:bg-[var(--color-accent-lightBlue)] hover:text-[var(--color-accent-blue)] ${isSidebarOpen && "w-full justify-start pl-5"}`}>
-					<FontAwesomeIcon icon={faHouse} />
-					{isSidebarOpen == true ? "home" : ""}
-				</div>
-				<div className={`capitalize w-10 h-10 rounded-full flex justify-center items-center gap-2 hover:bg-[var(--color-accent-lightBlue)] hover:text-[var(--color-accent-blue)] ${isSidebarOpen && "w-full justify-start pl-5"}`}>
-					<FontAwesomeIcon icon={faMagnifyingGlass} />
-					{isSidebarOpen == true ? "search" : ""}
-				</div>
-				<div className={`capitalize w-10 h-10 rounded-full flex justify-center items-center gap-2 hover:bg-[var(--color-accent-lightBlue)] hover:text-[var(--color-accent-blue)] ${isSidebarOpen && "w-full justify-start pl-5"}`}>
-					<FontAwesomeIcon icon={faGear} />
-					{isSidebarOpen == true ? "setting" : ""}
-				</div>
-				<div className={`capitalize w-10 h-10 rounded-full flex justify-center items-center gap-2 hover:bg-[var(--color-accent-lightBlue)] hover:text-[var(--color-accent-blue)] ${isSidebarOpen && "w-full justify-start pl-5"}`}>
-					<FontAwesomeIcon icon={faUser} />
-					{isSidebarOpen == true ? "user" : ""}
+		<>
+			<div id="overlay" className={`md:hidden absolute inset-0 z-50 ${isSidebarOpen ? "block bg-black/50" : "bg-none"}`}
+				onClick={() => setSidebarOpen(prev => !prev)}></div>
+			<div id="sideBar" className={`flex flex-col items-center gap-3 py-5 px-3 border-r-1 border-[var(--color-border-lightGray)] ease-in-out duration-500 fixed top-0 bottom-0 left-0 bg-white -translate-x-full z-100 md:relative md:translate-x-0 ${isSidebarOpen ? "translate-x-0 w-50" : "w-20"}`}>
+				<Link to="/">
+						{isSidebarOpen
+							? <div className="flex items-center h-15">
+								<img src={siteLogo} className="w-full h-full" />
+								<span className="text-[var(--color-accent-blue)] text-2xl">ReciPal</span>
+							</div>
+							: <img src={siteLogoWithText} />
+						}
+					</Link>
+				<div className="flex flex-col items-center gap-3 py-5 w-full">
+					<Link
+						to="/"
+						activeProps={{
+							className: "bg-[var(--color-accent-lightBlue)] text-[var(--color-accent-blue)]",
+						}}
+						className={`capitalize w-10 h-10 rounded-full flex justify-center items-center gap-2 hover:bg-[var(--color-accent-lightBlue)] hover:text-[var(--color-accent-blue)] ${isSidebarOpen && "w-full justify-start pl-5"}`}>
+						<FontAwesomeIcon icon={faHouse} />
+						{isSidebarOpen && "home"}
+					</Link>
+					<div className={`capitalize w-10 h-10 rounded-full flex justify-center items-center gap-2 hover:bg-[var(--color-accent-lightBlue)] hover:text-[var(--color-accent-blue)] ${isSidebarOpen && "w-full justify-start pl-5"}`}>
+						<FontAwesomeIcon icon={faMagnifyingGlass} />
+						{isSidebarOpen && "search"}
+					</div>
+					<div className={`capitalize w-10 h-10 rounded-full flex justify-center items-center gap-2 hover:bg-[var(--color-accent-lightBlue)] hover:text-[var(--color-accent-blue)] ${isSidebarOpen && "w-full justify-start pl-5"}`}>
+						<FontAwesomeIcon icon={faGear} />
+						{isSidebarOpen && "setting"}
+					</div>
+					<div className={`capitalize w-10 h-10 rounded-full flex justify-center items-center gap-2 hover:bg-[var(--color-accent-lightBlue)] hover:text-[var(--color-accent-blue)] ${isSidebarOpen && "w-full justify-start pl-5"}`}>
+						<FontAwesomeIcon icon={faUser} />
+						{isSidebarOpen && "user"}
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
